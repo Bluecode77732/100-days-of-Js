380e9ebd248ed99a68a9d81d51c028aa18bb1486
unknown <doookuuu@gmail.com>
2023-10-24 11:02:38 +0900

    WOP : Simple XO Game

--- Simple XO Game/index.js
+++ Simple XO Game/index.js
@@ -6,11 +6,11 @@ span = document.getElementsByTagName("span");
 restartButton = '<button onclick="playAgain()"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-repeat" viewBox="0 0 16 16"><path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"/><path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"/></svg></button>';
 
 function play(y) {
-    if(y.dataset.player == "none" && window.isGameOver == false) {
+    if (y.dataset.player == "none" && window.isGameOver == false) {
         y.innerHTML = playerTurn;
         y.dataset.player = playerTurn;
         moves++;
-    } else if(playerTurn == "0") {
+    } else if (playerTurn == "0") {
         playerTurn = "x";
     }
 
@@ -26,7 +26,7 @@ function play(y) {
 
     /* No Winner */
 
-    if(moves == 9 && isGameOver == false) {
+    if (moves == 9 && isGameOver == false) {
         draw();
     }
 }
@@ -35,7 +35,7 @@ function checkWinner(a, b, c) {
     a--;
     b--;
     c--;
-    if((span[a].dataset.player === span[b].dataset.player) && (span[b].dataset.player === span[c].dataset.player) && (span[a].dataset.player === span[c].dataset.player) && (span[a].dataset.player === "x" || span[a].dataset.player === "o") && isGameOver == false) {
+    if ((span[a].dataset.player === span[b].dataset.player) && (span[b].dataset.player === span[c].dataset.player) && (span[a].dataset.player === span[c].dataset.player) && (span[a].dataset.player === "x" || span[a].dataset.player === "o") && isGameOver == false) {
         span[a].parentNode.className += "activeBox";
         span[b].parentNode.className += "activeBox";
         span[c].parentNode.className += "activeBox";
@@ -47,8 +47,23 @@ function playAgain() {
     document.getElementsByClassName("alert")[0].parentNode.removeChild(document.getElementsByClassName("alert")[0]);
     resetGame();
     window.isGameOver = false;
-    for(let k = 0; k < span.length; k++) {
+    for (let k = 0; k < span.length; k++) {
         span[k].parantNode.className = span[k].parentNode.className.replace("activeBox", "");
     }
 }
 
+function resetGame() {
+    for (i = 0; i < span.length; i++) {
+        span[i].dataset.player = "none";
+        span[i].innerHTML = "&nbsp;";
+    }
+    playerTurn = "x";
+}
+
+function gameOver(a) {
+    let gameOverAlertElement = "<b>Game Over</b><br><br>Player" + span[a].dataset.player.toUpperCase();
+    let div = document.createElement("div");
+    div.className = "alert";
+    div.innerHTML = gameOverAlertElement;
+    document.getlementby
+}
