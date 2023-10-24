7c6b95ec47741afab53fb338f3c4ec11cd2fc8eb
Bluecode77732 <doookuuu@gmail.com>
2023-10-24 19:52:33 +0900

    FIN : Simple XO Game

--- Simple XO Game/index.js
+++ Simple XO Game/index.js
@@ -4,7 +4,6 @@ moves = 0;
 isGameOver = false;
 span = document.getElementsByTagName("span");
 restartButton = '<button onclick="playAgain()"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-repeat" viewBox="0 0 16 16"><path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"/><path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"/></svg></button>';
-
 function play(y) {
     if (y.dataset.player == "none" && window.isGameOver == false) {
         y.innerHTML = playerTurn;
@@ -18,7 +17,6 @@ function play(y) {
     }
 
     /* Win Types */
-
     checkWinner(1, 2, 3);
     checkWinner(4, 5, 6);
     checkWinner(7, 8, 9);
@@ -30,8 +28,9 @@ function play(y) {
 
     /* No Winner */
 
-    if (moves == 9 && isGameOver == false) { draw(); }
-
+    if (moves == 9 && isGameOver == false) {
+        draw();
+    }
 }
 
 function checkWinner(a, b, c) {
@@ -39,13 +38,12 @@ function checkWinner(a, b, c) {
     b--;
     c--;
     if ((span[a].dataset.player === span[b].dataset.player) && (span[b].dataset.player === span[c].dataset.player) && (span[a].dataset.player === span[c].dataset.player) && (span[a].dataset.player === "x" || span[a].dataset.player === "o") && isGameOver == false) {
-        span[a].parentNode.className += " activeBox";
-        span[b].parentNode.className += " activeBox";
-        span[c].parentNode.className += " activeBox";
+        span[a].parentNode.className += "activeBox";
+        span[b].parentNode.className += "activeBox";
+        span[c].parentNode.className += "activeBox";
         gameOver(a);
     }
 }
-
 function playAgain() {
     document.getElementsByClassName("alert")[0].parentNode.removeChild(document.getElementsByClassName("alert")[0]);
     resetGame();
@@ -64,7 +62,7 @@ function resetGame() {
 }
 
 function gameOver(a) {
-    var gameOverAlertElement = "<b>GAME OVER </b><br><br> Player " + span[a].dataset.player.toUpperCase() + ' Win !!! <br><br>' + restartButton;
+    var gameOverAlertElement = "<b>Game Over </b><br><br> Player " + span[a].dataset.player.toUpperCase() + 'Win <br><br>' + restartButton;
     var div = document.createElement("div");
     div.className = "alert";
     div.innerHTML = gameOverAlertElement;
@@ -74,7 +72,7 @@ function gameOver(a) {
 }
 
 function draw() {
-    var drawAlertElement = '<b>DRAW!!!</b><br><br>' + restartButton;
+    var drawAlertElement = '<b>DRAW</b><br><br>' + restartButton;
     var div = document.createElement("div");
     div.className = "alert";
     div.innerHTML = drawAlertElement;
