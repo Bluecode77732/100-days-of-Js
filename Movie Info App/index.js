e61282ca38e3b044ab90d4a6fe160caaf0cf9f3e
unknown <doookuuu@gmail.com>
2023-11-28 13:06:34 +0900

    New commit

--- Movie Info App/index.js
+++ Movie Info App/index.js
@@ -5,6 +5,14 @@ let result = document.getElementById("result");
 //function to fetch data from api
 let getMovie = () => {
     let movieName = movieNameRef.value;
-    let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}&`
+    let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}&`;
+
+    if(movieName <= 0) {
+        result.innerHTML = `<h3 class="msg">Please enter a movie name</h3>`;
+    } else {
+        fetch(url).then((res) => res.json()).then((data) => {
+            
+        })
+    }
 
 }
