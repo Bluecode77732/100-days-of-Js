1268f77b19b3537f1ca6d51f7a0d16773e9e7b5a
Bluecode77732 <doookuuu@gmail.com>
2023-12-12 22:30:54 +0900

    New commits

--- Translator/Js/script.js
+++ Translator/Js/script.js
@@ -6,5 +6,20 @@ const icons = document.querySelector('.row i');
 const translateBtn = document.querySelector("button");
 
 selectTag.forEach((tag, id) => {
-    
-})
+    for(let country_code in countries) {
+        let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "de-DE" ? "selected" : "";
+        let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
+        tag.insertAdjacentHTML("beforeend", option);
+    }
+});
+
+/* I don't understand this logic */
+exchangeIcon.addEventListener("click", () => {
+    let tempText = fromText.value, 
+        tempLang = selectTag[0].value;
+    fromText.value = toText.value;
+    toText.value = tempText;
+    selectTag[0].value = selectTag[1].value;
+    selectTag[1].value = tempLang;
+});
+
