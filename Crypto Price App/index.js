794ce1a84e7e58c23d26b636c9a19b89ff4ff045
Bluecode77732 <doookuuu@gmail.com>
2023-12-17 11:37:43 +0900

    Crypto Price App

---
+++ Crypto Price App/index.js
@@ -0,0 +1,31 @@
+fetch(
+  "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2C%20tether%2C%20ethereum%2C%20litecoin%2C%20cardano%2C%20dogecoin&vs_currencies=usd&include_24hr_change=true"
+)
+  .then((res) => res.json)
+  .then((json) => {
+    const container = document.querySelector(".contaier");
+    const coins = Object.getOwnPropertyNames(json);
+
+    for (let coin of coins) {
+      const coinInfo = json[`${coin}`];
+      const price = coinInfo.usd;
+      const change = coinInfo.usd_24hr_change.toFixed(5);
+
+      container.innerHTML += `
+            <div class="coin ${change < 0 ? "falling" : "rising"}"
+                <div class="coin-logo">
+                <img src="image/${coin}.png">
+            </div>
+            <div class="coin-name">
+                <h3>${coin}</h3>
+                <span>/USD/</span>
+            </div>
+                <div class="coin-price">
+                    <span class="price">$${price}</span>
+                    <span class="change">${change}</span>
+                </div>
+            </div>
+        `;
+    }
+  }
+);
