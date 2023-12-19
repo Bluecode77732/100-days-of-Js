315ae513346cf8cec7f59ca95c0be4cdf9e908ac
Bluecode77732 <doookuuu@gmail.com>
2023-12-19 19:56:03 +0900

    New commits

--- Crypto Price App/index.js
+++ Crypto Price App/index.js
@@ -1,5 +1,5 @@
 fetch(
-  "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2C%20tether%2C%20ethereum%2C%20litecoin%2C%20cardano%2C%20dogecoin&vs_currencies=usd&include_24hr_change=true"
+  `https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2C%20tether%2C%20ethereum%2C%20litecoin%2C%20cardano%2C%20dogecoin&vs_currencies=usd&include_24hr_change=true`
 )
   .then(res => res.json())
   .then(json => {
@@ -12,19 +12,19 @@ fetch(
       const change = coinInfo.usd_24h_change.toFixed(5);
 
       container.innerHTML += `
-            <div class="coin ${change < 0 ? "falling" : "rising"}">
-              <div class="coin-logo">
-                <img src="image/${coin}.png">
-              </div>
-              <div class="coin-name">
-                <h3>${coin}</h3>
-                <span>/USD/</span>
-              </div>
-                <div class="coin-price">
-                  <span class="price">$${price}</span>
-                  <span class="change">${change}</span>
-                </div>
-            </div>
-        `;
+        <div class="coin ${change < 0 ? 'falling' : 'rising'}">
+          <div class="coin-logo">
+            <img src="images/${coin}.png">
+          </div>
+          <div class="coin-name">
+            <h3>${coin}</h3>
+            <span>/USD</span>
+          </div>
+          <div class="coin-price">
+            <span class="price">$${price}</span>
+            <span class="change">${change}</span>
+          </div>
+        </div>
+      `
     }
-  });
+});
