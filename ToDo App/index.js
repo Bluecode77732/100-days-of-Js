47b173845052d669ae2ea66cc53eefd94eb59c4a
Bluecode77732 <doookuuu@gmail.com>
2023-12-28 21:48:00 +0900

    New commits

--- ToDo App/index.js
+++ ToDo App/index.js
@@ -3,3 +3,15 @@ const taskInput = document.querySelector(".task-input input"),
     clearAll = document.querySelector(".clear-btn"),
     taskBox = document.querySelector(".task-box");
 
+let editId,
+    isEditTask = false,
+    todos = JSON.parse(localStorage.getItem("todo-list"));
+
+filters.forEach(btn => {
+    btn.addEventListener("click", () => {
+        document.querySelector("span.active").classList.remove("active");
+        btn.classList.add("active");
+        showToDo(btn.id);
+    });
+});
+
