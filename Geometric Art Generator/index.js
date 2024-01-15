14c79269eeba50d76a9195e24635a1982d3ddf83
unknown <doookuuu@gmail.com>
2024-01-15 14:25:48 +0900

    comments

---
+++ Geometric Art Generator/index.js
@@ -0,0 +1,12 @@
+const btn = document.getElementById('btn');
+const shapes = [
+    "quad-circle-1",
+    "quad-circle-2",
+    "quad-circle-3",
+    "quad-circle-4",
+    "triangle-1",
+    "triangle-2",
+    "triangle-3",
+    "triangle-4",
+    "circle",
+];
