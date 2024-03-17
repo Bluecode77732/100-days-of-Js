c227322159b02db9ad1a4973b44d621bdb0f2bdf
Bluecode77732 <doookuuu@gmail.com>
2024-03-17 22:17:05 +0900

    New commits

--- Budget App/index.js
+++ Budget App/index.js
@@ -1,2 +1,27 @@
 let totalAmount = document.getElementById("total-amount");
 let userAmount = document.getElementById("user-amount");
+
+const totalAmountButton = document.getElementById("total-amount-button");
+
+const errorMessage = document.getElementById("budget-error");
+
+
+
+
+
+
+let tempAmount = 0;
+
+//Set Budget function
+
+totalAmount.addEventListener("click", () => {
+    tempAmount = totalAmount.value();
+    //Bad input
+    if (tempAmount === "" || tempAmount < 0) {
+        errorMessage.classList.remove("hide");
+    } else {
+        errorMessage.classList.add("hide");
+        //Set budget
+        
+    }
+})
