20ea5cfc9c5a972e88f1bc09b3047765ad07b625
Bluecode77732 <doookuuu@gmail.com>
2024-03-19 22:07:14 +0900

    New commits

--- Budget App/index.js
+++ Budget App/index.js
@@ -28,3 +28,31 @@ totalAmount.addEventListener("click", () => {
         totalAmount.value = "";
     }
 });
+
+//Disable edit and delete button function
+
+const disableButtons = (bool) => { 
+    let editButtons = document.getElementById("edit");
+    Array.from(editButtons).forEach((element) => {
+        element.disabled = bool;    //disabled??
+    });
+}
+
+// Modify list elements function
+
+const modifyElement = (elem, edit = false) => {
+    let parentDiv = elem.parentElement;
+    let currentBalance = balanceValue.innerText;
+    let currentExpense = expenditureValue.innerText;
+    let parentAmount = parentDiv.querySelector(".amount").innerText;
+    if (edit) {
+        let parentText = parentDiv.querySelector(".product").innerText;
+        productTitle.value = parentText;
+        userAmount.value = parentAmount;
+        disableButtons(true);
+    }
+
+    balanceValue.innerText = parseInt(currentBalance) + parseInt(parentAmount);
+    expenditureValue.innerText = parseInt(currentExpense) 
+}
+
