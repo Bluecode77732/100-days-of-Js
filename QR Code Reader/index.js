87dcda2a5d24b389ce9f91fdd82638f9978a1cd5
Bluecode77732 <doookuuu@gmail.com>
2024-04-13 20:07:15 +0900

    QR Code Reader

---
+++ QR Code Reader/index.js
@@ -0,0 +1,9 @@
+const wrapper = document.querySelector(".wrapper");
+const form = document.querySelector("form");
+const FileInp = document.querySelector("input");
+const infoText = document.querySelector("p");
+const closeBtn = document.querySelector(".close");
+const copyBtn = document.querySelector(".copy");
+
+// 
+
