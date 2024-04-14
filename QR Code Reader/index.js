b7235a572f393b3e7d0552278155cb6bc335234d
Bluecode77732 <doookuuu@gmail.com>
2024-04-14 15:29:51 +0900

    New commits

--- QR Code Reader/index.js
+++ QR Code Reader/index.js
@@ -7,7 +7,21 @@ const copyBtn = document.querySelector(".copy");
 
 // Fetch Data from the Application Program Interface
 
-function fetchReq(file, formFata) {
+function fetchReq(file, formData) {
     infoText.innerText = "Scanning QR Code...";
-    fetch("")
+    fetch("http://api.qrserver.com/v1/create-qr-code/?", {
+        method: 'POST', body: formData
+    }).then(
+        res => res.json()
+    ).then(
+        result => {
+            result = result[0].symbol[0].data;
+            infoText.innerText = result ? "Upload QR Code To Scan" : " Couldn't Scan QR Code";
+            if(!result) return;
+            document.querySelector("textarea").innerText = result;
+            form.querySelector("img").src = URL.createObjectURL(file);
+            wrapper.classList.add("active");
+        }).catch(() => {
+            infoText.innerText = "Couldn't Scan QR Code...";
+    });
 }
