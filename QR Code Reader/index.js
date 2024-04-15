fabe45256d86e3bd7195229a52566908b066d3ed
unknown <doookuuu@gmail.com>
2024-04-15 12:53:19 +0900

    New comment

--- QR Code Reader/index.js
+++ QR Code Reader/index.js
@@ -10,18 +10,46 @@ const copyBtn = document.querySelector(".copy");
 function fetchReq(file, formData) {
     infoText.innerText = "Scanning QR Code...";
     fetch("http://api.qrserver.com/v1/create-qr-code/?", {
-        method: 'POST', body: formData
-    }).then(
-        res => res.json()
-    ).then(
-        result => {
+        method: "POST",
+        body: formData,
+    })
+    .then((res) => res.json())
+    .then((result) => {
             result = result[0].symbol[0].data;
-            infoText.innerText = result ? "Upload QR Code To Scan" : " Couldn't Scan QR Code";
-            if(!result) return;
+            infoText.innerText = result
+                ? "Upload QR Code To Scan"
+                : " Couldn't Scan QR Code";
+            if (!result) return;
             document.querySelector("textarea").innerText = result;
             form.querySelector("img").src = URL.createObjectURL(file);
             wrapper.classList.add("active");
-        }).catch(() => {
+        })
+        .catch(() => {
             infoText.innerText = "Couldn't Scan QR Code...";
-    });
+        });
 }
+
+// Send QR Code file with Request To API
+
+FileInp.addEventListener("change", async e => {
+    let file = e.target.files[0];
+    if(!file) return;
+    let formData = new FormData();
+    formData.append('file', file);
+    fetchReq(file, formData)
+});
+
+// Copy text to clipboard
+
+copyBtn.addEventListener("click", () => {
+    let txt = document.querySelector("textarea").textContent;
+    navigator.clipboard.writeText(txt);
+});
+
+// When user click on 'form' perform 'fileInp' Eventlistener function
+
+form.addEventListener("click", () => {
+    FileInp.click()
+});
+
+closeBtn.addEventListener("click", () => wrapper.classList.remove("active"));
