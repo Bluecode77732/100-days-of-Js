cad543bda9421821993bebe3136a82b4f74ac6c4
unknown <doookuuu@gmail.com>
2024-05-02 13:00:03 +0900

    New comment

---
+++ Guess The Word Game/index.js
@@ -0,0 +1,10 @@
+const input = document.querySelector(".word"),
+    hintTag = document.querySelector(".hint span"),
+    guessLeft = document.querySelector(""),
+    mistakes = document.querySelector(""),
+    resetBtn = document.querySelector(""),
+    hintBtn = document.querySelector(""),
+    hintElement = document.querySelector(""),
+    typeInput = document.querySelector("");
+
+// 
