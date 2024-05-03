a151cac9cdef31d2e13985919b61c2c8f86b607a
unknown <doookuuu@gmail.com>
2024-05-03 13:02:04 +0900

    New comment

--- Guess The Word Game/index.js
+++ Guess The Word Game/index.js
@@ -1,10 +1,19 @@
 const input = document.querySelector(".word"),
     hintTag = document.querySelector(".hint span"),
-    guessLeft = document.querySelector(""),
-    mistakes = document.querySelector(""),
-    resetBtn = document.querySelector(""),
-    hintBtn = document.querySelector(""),
-    hintElement = document.querySelector(""),
-    typeInput = document.querySelector("");
+    guessLeft = document.querySelector("guess span"),
+    mistakes = document.querySelector(".wrong span"),
+    resetBtn = document.querySelector(".reset"),
+    hintBtn = document.querySelector("showHint"),
+    hintElement = document.querySelector(".hint"),
+    typeInput = document.querySelector(".type-input");
 
-// 
+// Initializing game varibles
+let word, incorrectLetters = [], correctLetters = [], maxGuesses;
+
+// Selecct random word from word list and set up game
+function startNewGame() {
+    alert("New Game Started. Guess New Word!");
+    // Hide hint element
+    hintElement.style.display = "none";
+    hintElement.style.opacity = "0";
+}
