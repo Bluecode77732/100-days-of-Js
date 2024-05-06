f80b90b7154de35a0630697f30a14224ed22ca1f
Bluecode77732 <doookuuu@gmail.com>
2024-05-06 21:11:18 +0900

    Debug

--- Gradient Generator/index.js
+++ Gradient Generator/index.js
@@ -1,6 +1,6 @@
 let colorOne = document.getElementById("color-a");
 let colorTwo = document.getElementById("color-b");
-let currentDireciton = 'to bottom';
+let currentDirection = 'to bottom';
 let outputCode = document.getElementById("code");
 
 function setDirections(value, _this) {
@@ -9,12 +9,12 @@ function setDirections(value, _this) {
         i.classList.remove("active");
     }
     _this.classList.add("active")
-    currentDireciton = value;
+    currentDirection = value;
 }
 
 function generateCode() {
-    outputCode.value = `background-image : linear-gradient(${currentDireciton}, ${colorOne.value}, ${colorTwo.value})`;
-    document.getElementsByTagName("BODY"/* What if it's "body"? */)[0].style.backgroundImage = `linear-gradient(${currentDireciton}, ${colorOne.value}, ${colorTwo.value})`;
+    outputCode.value = `background-image : linear-gradient(${currentDirection}, ${colorOne.value}, ${colorTwo.value})`;
+    document.getElementsByTagName("BODY"/* What if it's "body"? */)[0].style.backgroundImage = `linear-gradient(${currentDirection}, ${colorOne.value}, ${colorTwo.value})`;
 }
 
 function copyText() {
