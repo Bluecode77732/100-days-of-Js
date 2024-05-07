da9cb0027c7fbc9983a55cdb56e2e58924fef288
unknown <doookuuu@gmail.com>
2024-05-07 12:57:38 +0900

    New comment

--- Guess The Word Game/index.js
+++ Guess The Word Game/index.js
@@ -16,4 +16,18 @@ function startNewGame() {
     // Hide hint element
     hintElement.style.display = "none";
     hintElement.style.opacity = "0";
+
+    // Choose random word from db and setup game
+    const ranWord = wordList[Math.floor(Math.random() * wordList.length)];
+    word = ranWord.word;
+    // If word charactors >= 5 then max guess = 8 else max guess = 6
+    maxGuesses = word.length >= 5 ? 8 : 6;
+    incorrectLetters = [];
+    correctLetters = [];
+    hintTag.innerText = ranWord.hint;
+    guessLeft.innerText = maxGuesses;
+    mistakes.innerText = incorrectLetters;
+
+    
 }
+
