b640e882788013dcd1194ebacb9c65e15b124ab4
unknown <doookuuu@gmail.com>
2024-05-16 12:55:52 +0900

    New comment

---
+++ Simon Game/index.js
@@ -0,0 +1,7 @@
+// get DOM elements needed for game
+const scoreEL = document.getElementById("score");
+const colorParts = document.querySelectorAll(".colors");
+const containerEl = document.querySelector(".container");
+const 
+const 
+const 
