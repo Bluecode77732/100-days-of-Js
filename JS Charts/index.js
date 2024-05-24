7f7828fd15b28fa825017f5bdbc12ce74da142e5
unknown <doookuuu@gmail.com>
2024-05-24 13:00:01 +0900

    new comment

--- JS Charts/index.js
+++ JS Charts/index.js
@@ -11,6 +11,30 @@ const chartOptions = {
     dataLabels : { enabled : false }, //Hide chart data labels
     stroke : { width : 3, curve : 'smooth' }, //Set chart stroke propeties
     fill : {
-        
-    }
-}
+        type : 'gradient',
+        gradient : {
+            shadeIntensity : 1,
+            opacityFrom : 0.7,
+            opacityTo : 0,
+            stops : [0, 90, 100] // Set chart fill gradient stops    
+        }
+    }, 
+    xaxis : {
+        categories : ['Feb', 'Apr', 'June', 'Aug', 'Oct', 'Dec'], //Set x-axis categories
+        axisBorder : { show : false }, // Hide x-axis border
+        labels : { style : { colors : '#a7a7a7', fontFamily : 'Poppins' } } // Set x-axis label propeties
+    },
+    yaxis : { show : false }, //Hide y-axis
+    grid : {
+        borderColor : 'rgba(0, 0, 0, 0)', // Set grid border color
+        padding : { top : -30, bottom : -8, left : 12, right : 12 } //Set grid padding
+    },
+    tooltip : {
+        enabled : true, // Enable chart tooltip
+        y : { formatter : value => `${value}K` }, // Set y-axis tooltip label formatter
+        style : { fontFamily : 'Poppins' } // Set tooltip font family
+    },
+    markers : { show : false }, // Hide chart markers
+};
+
+// Create new ApexCharts
