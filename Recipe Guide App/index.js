c90577552b45ce9cdef02058562b04ad00fab741
Bluecode77732 <doookuuu@gmail.com>
2024-06-12 22:29:09 +0900

    New commits

--- Recipe Guide App/index.js
+++ Recipe Guide App/index.js
@@ -18,7 +18,46 @@ function searchMeal() {
         return;
     }
     // Fetch meal data using api with user input
-    
+    fetch(apiUrl + userInput).then((response) => response.json()).then((data) => {
+        const meal = data.meals[0];
+        // Handle where no meal found
+        if(!meal) {
+            resultContainer.innerHTML = `<h3>No Meal Found. Please Try Again.</h3>`
+            return;
+        }
+        const ingredients = getIngredients(meal);
+        // Generate HTML to display meal data
+        const recipeHtml = `
+            <div class="details">
+                <h2>${meal.strMeal}</h2>
+                <h4>${meal.strArea}</h4>
+            </div>
+            <img src=${meal.strMealThumb} alt=${meal.strMeal} />
+            <div id="ingre-container">
+                <h3>Ingredients:</h3>
+                <ul>${ingredients}</ul>
+            </div>
+            <div id="recipe">
+                <button id="hide-recipe">X</button>
+                <pre id="instructions">${meal.strInstructions}</pre>
+            </div>
+            <button id="show-recipe">View Recipe</button>
+        `;
+        resultContainer.innerHTML = recipeHtml;
+
+        const hideRecipeBtn = document.getElementById("hide-recipe");
+        hideRecipeBtn.addEventListener("click", hideRecipe);
+        const showRecipeBtn = document.getElementById("show-recipe");
+        showRecipeBtn.addEventListener("click", showRecipe);
+        searchContainer.style.opacity = "0";
+        searchContainer.style.display = "none";
+    })
+    // Handle error
+    .catch(() => {
+        searchContainer.style.opacity = "1";
+        searchContainer.style.display = "grid";
+        resultContainer.innerHTML = `<h3>Fetching Data Error</h3>`
+    });    
 }
 
 // Generate HTML for list of ingredients
