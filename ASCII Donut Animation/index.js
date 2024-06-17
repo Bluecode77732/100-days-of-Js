75766eaeca3304ffee9342d8b95c428cbf95b0fa
unknown <doookuuu@gmail.com>
2024-06-17 12:58:56 +0900

    new comment

---
+++ ASCII Donut Animation/index.js
@@ -0,0 +1,32 @@
+(function () {
+    let preTag = document.getElementById("donut");
+
+    // Angles, Radius and Contants
+    let A = 1;
+    let B = 1;
+    let R1 = 1;
+    let R2 = 2;
+    let K1 = 150;
+    let K2 = 5;
+
+    // Function to render ASCII frame
+    function renderAscii() {
+        let b = [];     // Array to stay ascii chars
+        let z = [];     // Array to store ascii values
+
+        let width = 280;    // Width of frame    
+        let height = 280;    // Height of frame    
+
+
+        A += 0.07;   // Increament angle a 
+        B += 0.07;   // Increament angle b 
+        // Sin and Cosine of angles
+        let sA = Math.cos(A),
+            cA = Math.cos(A),
+            sB = Math.cos(b),
+            cB = Math.cos(b);
+
+        // Initialize arrays with default angles
+        
+    }
+});
