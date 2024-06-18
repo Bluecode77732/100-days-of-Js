44e6a7df5f098e3222763bf1dd21d5d45be62dd6
Bluecode77732 <doookuuu@gmail.com>
2024-06-18 12:02:17 +0900

    New commits

--- githubAPI/index.js
+++ githubAPI/index.js
@@ -1,4 +1,8 @@
 const searchGithub = async () => {
-    const username = document.getElementById("searchInput").ariaValueMax;
-    
+    const username = document.getElementById("searchInput").value;
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    const detailsContainer =  document.querySelector(".details");
+    const data = await response.json();
 }
+
+
